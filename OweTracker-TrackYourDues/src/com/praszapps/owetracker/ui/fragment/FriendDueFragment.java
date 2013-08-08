5379da45461ac0eb4a69483b798b9a6e780be10a
Prasannajeet Pani <me@prasannajeetpani.in>
2013-08-09 00:34:26 +0530

    Removed unused imports
    Signed-off-by: Prasannajeet Pani <me@prasannajeetpani.in>

--- OweTracker-TrackYourDues/src/com/praszapps/owetracker/ui/fragment/FriendDueFragment.java
+++ OweTracker-TrackYourDues/src/com/praszapps/owetracker/ui/fragment/FriendDueFragment.java
@@ -22,7 +22,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnFocusChangeListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;

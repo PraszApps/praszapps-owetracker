8f27327cd090acd13d26b180117947639e545d79
ravi.paduchuri <ravi.paduchuri@gmail.com>
2013-09-03 15:04:49 +0530

    Application class added to access contexts

---
+++ OweTracker-TrackYourDues/src/com/praszapps/owetracker/application/OweTrackerApplication.java
@@ -0,0 +1,20 @@
+package com.praszapps.owetracker.application;
+
+import android.app.Application;
+import android.content.Context;
+
+public class OweTrackerApplication extends Application {
+
+	private static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = this;
+    }
+
+    public static Context getContext(){
+        return mContext;
+    }
+	
+}
